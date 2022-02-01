9c06192a1bf72fe37173bff4096e5cb784fc5737
HuantWang <wanghuanting@stumail.nwu.edu.cn>
2022-02-02 05:43:32 +0800

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-
+if 'READTHEDOCS' not in os.environ:
+    import cython_generated_ext
 # -- Project information
 project = "SuperSonic"
 copyright = "University of Leeds, Northwest University"
